eadde54817bf736a13355f5987797df3f0325691
Manjiri-Boramanikar <63940355+Manjiri-Boramanikar@users.noreply.github.com>
2022-12-20 12:01:45 +0530

    Create Calc.py

---
+++ Calc.py
@@ -0,0 +1,10 @@
+no1 = int(input("Enter first numbber : ")
+no2 = int(input("Enter second number : ")
+multiplication = 0
+division = 0
+
+multiplication = no1*no2
+division = no1/no2
+
+print("The multiplication of two numbers is : ", multiplication)
+print("The division of two numbers is : ",division)
